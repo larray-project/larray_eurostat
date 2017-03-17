43814150cc06de4c90e0402b37d2ec11786ba9de
alixdamman <ald@plan.be>
2017-03-17 10:06:30 +0100

    fix #2 : update eurostat_get function

--- larray_eurostat/tsv.py
+++ larray_eurostat/tsv.py
@@ -1,52 +1,33 @@
 from __future__ import absolute_import, division, print_function
 
-import os
 import urllib.request
 import gzip
-import larray as la
 
-__all__ = ["eurostat_get"]
+from larray import read_eurostat
+from larray.utils import StringIO
 
-_eurostat_path = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=data%2F"
-
-
-def _cleantsv(tsv):
-    fline = tsv.index('\n')
-    header = tsv[:fline]
-    contents = tsv[fline:].translate({ord(i): None for i in ' dbefcuipsrzn:'})
-    return (header + contents)
+EUROSTAT_BASEURL = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=data%2F"
 
 
-def eurostat_get(indicator, path='', drop_tsv=True):
-    '''Gets Eurostat indicator and returns it as an array.
+def eurostat_get(indicator, drop_markers=True):
+    """Gets Eurostat indicator and returns it as an array.
 
-    Paramaters
+    Parameters
     ----------
     indicator : str
-        eurostat indicator
-    path : str
-        path to store temporary files (default: current directory)
-    drop_tsv : bool
-        delete tsv-file afterwards
+        name of eurostat indicator
+    drop_markers : bool
+        drop markers.
 
     Returns
     -------
     LArray
-        Array containing indicator.
-    '''
-    tsv = indicator + ".tsv"
-    gz = tsv + ".gz"
-    url = _eurostat_path + gz
-    file_name, headers = urllib.request.urlretrieve(url, path + gz)
-
-    fgz = gzip.open(path + gz, 'rt')
-    ftsv = open(path + tsv, 'w+')
-    tsvbuffer = fgz.read()
-    ftsv.write(_cleantsv(tsvbuffer))
-    fgz.close()
-    ftsv.close()
-    os.remove(path + gz)
-    la_data = la.read_eurostat(path + tsv)
-    if drop_tsv:
-        os.remove(path + tsv)
-    return la_data
+    """
+    with urllib.request.urlopen(EUROSTAT_BASEURL + indicator + ".tsv.gz") as f:
+        with gzip.open(f, mode='rt') as fgz:
+            s = fgz.read()
+            if drop_markers:
+                first_line_end = s.index('\n')
+                # strip markers except on first line
+                s = s[:first_line_end] + s[first_line_end:].translate({ord(i): None for i in ' dbefcuipsrzn:'})
+            return read_eurostat(StringIO(s))
