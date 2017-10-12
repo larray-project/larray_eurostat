9c7922cdc9a13628e428d857d82a1773064d4d38
Alix Damman <ald@plan.be>
2017-10-12 09:45:21 +0200

    bump to version 0.26

--- larray_eurostat/__init__.py
+++ larray_eurostat/__init__.py
@@ -1,3 +1,5 @@
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import, print_function
 
-from larray_eurostat.tsv import *
+from larray_eurostat.tsv import eurostat_get
+
+__version__ = "0.26"
