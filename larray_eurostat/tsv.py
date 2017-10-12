5e4da4f75c6f8ae7ae0ac56927bd9712cbf1f346
Alix Damman <ald@plan.be>
2017-10-12 09:39:43 +0200

    fix #6 : changed import of StringIO + added doctest to eurostat_get

--- larray_eurostat/tsv.py
+++ larray_eurostat/tsv.py
@@ -1,10 +1,15 @@
 from __future__ import absolute_import, division, print_function
 
+import sys
 import urllib.request
 import gzip
 
 from larray import read_eurostat
-from larray.utils import StringIO
+
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
 
 EUROSTAT_BASEURL = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=data%2F"
 
@@ -22,6 +27,15 @@ def eurostat_get(indicator, drop_markers=True):
     Returns
     -------
     LArray
+    
+    Examples
+    --------
+    >>> gdp = eurostat_get('nama_aux_cra')
+    >>> gdp.info
+    1 x 45 x 54
+     indic_na [1]: 'PPS_NAC'
+     geo [45]: 'AT' 'BA' 'BE' ... 'TR' 'UK' 'US'
+     time [54]: 2013 2012 2011 ... 1962 1961 1960
     """
     with urllib.request.urlopen(EUROSTAT_BASEURL + indicator + ".tsv.gz") as f:
         with gzip.open(f, mode='rt') as fgz:
