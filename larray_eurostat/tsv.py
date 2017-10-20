9ec0994356da97034eedeef3084f6411c5a7f29e
Gaëtan de Menten <gdementen@gmail.com>
2017-10-20 11:02:54 +0200

    implemented getting several indicators at once

--- larray_eurostat/tsv.py
+++ larray_eurostat/tsv.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import sys
 import gzip
 
-from larray import read_eurostat
+from larray import read_eurostat, Session
 
 if sys.version_info[0] < 3:
     from StringIO import StringIO
@@ -34,31 +34,9 @@ else:
 EUROSTAT_BASEURL = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&file=data%2F"
 
 
-def eurostat_get(indicator, drop_markers=True):
-    """Gets Eurostat indicator and returns it as an array.
+def _get_one(indicator, drop_markers=True):
+    """Get one Eurostat indicator and return it as an array"""
 
-    Parameters
-    ----------
-    indicator : str
-        name of eurostat indicator
-    drop_markers : bool
-        drop markers.
-
-    Returns
-    -------
-    LArray
-
-    Examples
-    --------
-    >>> data = eurostat_get('nama_10r_2gvagr')
-    >>> data.info
-    1 x 310 x 16
-     unit [1]: 'PCH_PRE'
-     geo [310]: 'AT' 'BE' 'BE1' ... 'UKN0' 'UKZ' 'UKZZ'
-     time [16]: 2015 2014 2013 ... 2002 2001 2000
-    dtype: float64
-    memory used: 38.75 Kb
-    """
     with urlopen(EUROSTAT_BASEURL + indicator + ".tsv.gz") as f:
         with gzip_open(f, mode='rt') as fgz:
             try:
@@ -72,3 +50,53 @@ def eurostat_get(indicator, drop_markers=True):
                 if sys.version_info[0] >= 3:
                     e.args = (e.args[0] + " \nCan't open file {}".format(f.geturl()),)
                 raise
+
+
+def eurostat_get(indicators, drop_markers=True):
+    """Gets one or several Eurostat indicators and return them as an array or a session.
+
+    Parameters
+    ----------
+    indicators : str or list/tuple of str
+        Name(s) of eurostat indicator(s). When requesting a single indicator, the result is an array, otherwise it is a
+        Session.
+    drop_markers : bool
+        drop markers.
+
+    Returns
+    -------
+    LArray or Session
+
+    Examples
+    --------
+    >>> data = eurostat_get('avia_ec_enterp')
+    >>> data.info
+    2 x 16 x 13
+     enterpr [2]: 'AIRP' 'AVIA'
+     geo [16]: 'CY' 'CZ' 'EE' ... 'BG' 'FI' 'SE'
+     time [13]: 2013 2012 2011 ... 2003 2002 2001
+    dtype: float64
+    memory used: 3.25 Kb
+    >>> indicators = eurostat_get(['avia_ec_enterp', 'apro_mt_lsequi'])
+    >>> indicators.names
+    ['apro_mt_lsequi', 'avia_ec_enterp']
+    >>> indicators.avia_ec_enterp.info
+    2 x 16 x 13
+     enterpr [2]: 'AIRP' 'AVIA'
+     geo [16]: 'CY' 'CZ' 'EE' ... 'BG' 'FI' 'SE'
+     time [13]: 2013 2012 2011 ... 2003 2002 2001
+    dtype: float64
+    memory used: 3.25 Kb
+    >>> indicators.apro_mt_lsequi.info
+    3 x 1 x 28 x 38
+     animals [3]: 'A1000' 'A1100' 'A1200'
+     unit [1]: 'THS_HD'
+     geo [28]: 'AL' 'BE' 'BG' ... 'SI' 'SK' 'UK'
+     time [38]: 1997 1996 1995 ... 1962 1961 1960
+    dtype: float64
+    memory used: 24.94 Kb
+    """
+    if isinstance(indicators, (tuple, list)):
+        return Session([(i, _get_one(i, drop_markers=drop_markers)) for i in indicators])
+    else:
+        return _get_one(indicators, drop_markers=drop_markers)
