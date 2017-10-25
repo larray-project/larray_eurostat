1894d97d68de987a917ab7299ed34eb6a16a630c
Alix Damman <ald@plan.be>
2017-10-25 09:16:45 +0200

    bump to version 0.26.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray_eurostat'
-VERSION = '0.26'
+VERSION = '0.26.1'
 AUTHOR = 'Alix Damman, Gaetan de Menten, Geert Bryon, Johan Duyck'
 AUTHOR_EMAIL = 'ald@plan.be'
 DESCRIPTION = "Additional package to import Eurostat files using LArray"
