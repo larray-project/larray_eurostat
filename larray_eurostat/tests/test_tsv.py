6d0c19be752c2da7d3d8419a0d5f33abe7cc1b39
Alix Damman <ald@plan.be>
2020-02-27 15:57:00 +0100

    fix #18 : dropped Python 2 support

--- larray_eurostat/tests/test_tsv.py
+++ larray_eurostat/tests/test_tsv.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
 import pytest
 import re
 from larray_eurostat.tsv import *
@@ -8,10 +6,6 @@ from larray_eurostat.tsv import *
 def test_eurostat_get():
     dataset = 'nama_aux_cra'
 
-    msg = "Not a gzipped file"
-    if sys.version_info[0] >= 3:
-        msg += " (b'<!')\nCan't open file {}data/{}.tsv.gz".format(EUROSTAT_BASEURL, dataset)
-    type_err = IOError if sys.version_info[0] < 3 else OSError
-
-    with pytest.raises(type_err, match=re.escape(msg)):
+    msg = "Not a gzipped file (b'<!')\nCan't open file {}data/{}.tsv.gz".format(EUROSTAT_BASEURL, dataset)
+    with pytest.raises(OSError, match=re.escape(msg)):
         eurostat_get(dataset)
