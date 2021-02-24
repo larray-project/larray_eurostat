dbcd1a35134daf1a9807b28c858bd36e70f88f06
Gaëtan de Menten <gdementen@gmail.com>
2021-02-24 18:23:35 +0100

    use f-strings everywhere

--- larray_eurostat/tests/test_tsv.py
+++ larray_eurostat/tests/test_tsv.py
@@ -6,6 +6,6 @@ from larray_eurostat.tsv import *
 def test_eurostat_get():
     dataset = 'nama_aux_cra'
 
-    msg = "Not a gzipped file (b'<!')\nCan't open file {}data/{}.tsv.gz".format(EUROSTAT_BASEURL, dataset)
+    msg = f"Not a gzipped file (b'<!')\nCan't open file {EUROSTAT_BASEURL}data/{dataset}.tsv.gz"
     with pytest.raises(OSError, match=re.escape(msg)):
         eurostat_get(dataset)
