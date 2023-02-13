695b08c136f47fe3181f58d37221534cc7ab67a2
Gaëtan de Menten <gdementen@gmail.com>
2023-02-13 12:11:14 +0100

    TEST: renamed test for clarity

--- larray_eurostat/tests/test_tsv.py
+++ larray_eurostat/tests/test_tsv.py
@@ -3,7 +3,7 @@ import re
 from larray_eurostat.tsv import *
 
 
-def test_eurostat_get():
+def test_eurostat_get_bad_dataset():
     dataset = 'nama_aux_cra'
 
     msg = f"Not a gzipped file (b'<!')\nCan't open file {EUROSTAT_BASEURL}data/{dataset}.tsv.gz"
