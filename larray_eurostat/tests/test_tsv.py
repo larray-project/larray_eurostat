7402c3cd0cdfb0d24fa89a297f89128cb01a0547
Gaëtan de Menten <gdementen@gmail.com>
2023-09-12 13:11:52 +0200

    TEST: fixed broken test
    (no longer produced the same error)

--- larray_eurostat/tests/test_tsv.py
+++ larray_eurostat/tests/test_tsv.py
@@ -1,13 +1,14 @@
 import re
+from urllib.error import HTTPError
 
 import pytest
 
-from larray_eurostat.tsv import eurostat_get, EUROSTAT_BASEURL
+from larray_eurostat.tsv import eurostat_get
 
 
 def test_eurostat_get_bad_dataset():
-    dataset = 'nama_aux_cra'
+    dataset = 'does-not-exist'
 
-    msg = f"Not a gzipped file (b'<!')\nCan't open file {EUROSTAT_BASEURL}data/{dataset}.tsv.gz"
-    with pytest.raises(OSError, match=f'^{re.escape(msg)}$'):
+    msg = "HTTP Error 404: Not Found"
+    with pytest.raises(HTTPError, match=f'^{re.escape(msg)}$'):
         eurostat_get(dataset)
