c55b107cb9f853d64207592c41c83a917aacb111
Gaëtan de Menten <gdementen@gmail.com>
2025-12-03 13:13:32 +0100

    bump version to 0.35-dev

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray_eurostat'
-VERSION = '0.34.6'
+VERSION = '0.35-dev'
 AUTHOR = 'Alix Damman, Gaetan de Menten, Geert Bryon, Johan Duyck'
 AUTHOR_EMAIL = 'ald@plan.be'
 DESCRIPTION = "Additional package to import Eurostat files using LArray"
